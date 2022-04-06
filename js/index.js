9a7320c0bd04cc7f359916b9600c837d79effed1
unknown <juaestnino@uniboyaca.edu.co>
2022-04-05 21:58:11 -0500

    configuracion añadir html

--- js/index.js
+++ js/index.js
@@ -12,8 +12,28 @@ $(document).ready(()=>{
             //$(this).html("Mostrar h1");
         }
     });
+    var nuevoS = $('<section id="tema" class="seccion_tema">'+
+    '<article class="titulo_contenido">'+
+        '<input id="CheckT" type="checkbox">'+
+        '<h2 class="titulotm">'+$("#tituloCInput").val() +'</h2>'+
+        '<button id="ocultarBtn" class="boton_slidedown">'+
+          '<lord-icon'+
+            ' id="iconobtno"'+
+            ' src="https://cdn.lordicon.com/eflfmgmj.json"'+
+            ' trigger="hover"'+
+            ' style="width: 25px; height: 25px">'+
+          '</lord-icon>'+
+        '</button>'+
+      '</article>'+
+      '<article class="descripciont">'+
+        '<p>'+$("#descricionCInput").val()+
+        '</p>'+
+        '<a id="abrirVermas">ver más</a>'+
+      '</article>' +
+      '</section>');
     $("#guardarT").click(function(){
-        $("body").html("<strong>Hola!!!!</strong>");
+        $(".bodyPage").append(nuevoS);
+        $(".fondo_modalC").css("display","none");
     });
     $("#btncrearT").click(function(){
         $(".fondo_modalC").css("display","block");
